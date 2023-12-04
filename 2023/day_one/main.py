0b14f6ab3d3396d5c931429b184e6feaee564ffc
aripo99 <a_polakof@umail.ucsb.edu>
2023-12-03 18:34:59 -0800

    Part 2

--- 2023/day_one/main.py
+++ 2023/day_one/main.py
@@ -1,7 +1,33 @@
+import re
+
+
+def convert_spelled_numbers_to_digits(string: str) -> str:
+    number_mapping = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+
+    pattern = "|".join(number_mapping.keys())
+
+    def replace_spelled_numbers(match):
+        return number_mapping[match.group(0)]
+
+    return re.sub(pattern, replace_spelled_numbers, string, flags=re.IGNORECASE)
+
+
 def compute_calibration_values_sum(data):
     sum = 0
     for line in data:
-        numbers = [int(c) for c in line if c.isdigit()]
+        parsed_line = convert_spelled_numbers_to_digits(line)
+        numbers = [int(c) for c in parsed_line if c.isdigit()]
         if len(numbers) == 1:
             sum += numbers[0] * 10 + numbers[0]
         else:
