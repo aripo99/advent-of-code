574ea17985838f61cf276b497e831a7260298fc7
aripo99 <a_polakof@umail.ucsb.edu>
2023-12-03 19:08:42 -0800

    Day 2 part 2

--- 2023/day_two/main.py
+++ 2023/day_two/main.py
@@ -21,8 +21,7 @@ def parse_file(file_path):
     return games
 
 
-def main():
-    games = parse_file("2023/day_two/input.txt")
+def part_one(games):
     sum_indexes = 0
     num_cubes = {"red": 12, "green": 13, "blue": 14}
 
@@ -38,6 +37,31 @@ def main():
             sum_indexes += index + 1
 
     print(sum_indexes)
+
+
+def part_two(games):
+    result = 0
+    for game in games:
+        max_values = {"red": 0, "green": 0, "blue": 0}
+        for round in game:
+            for color, count in round[0].items():
+                if count > max_values[color]:
+                    max_values[color] = count
+
+        power_round_value = 1
+        for color, count in max_values.items():
+            power_round_value *= count
+
+        result += power_round_value
+
+    print(result)
+
+
+def main():
+    games = parse_file("2023/day_two/test.txt")
+
+    part_one(games)
+    part_two(games)
 
 
 if __name__ == "__main__":
